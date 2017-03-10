fc0413b3786e1a29f0f81d847829220ffad5d5ca
mrtarunkumar <Tarunkumar.Vegamwala@experian.com>
2017-03-10 15:26:39 +0000

    objects and arrays

---
+++ main.js
@@ -0,0 +1,14 @@
+// objects
+var myCat = {
+    "name": "Meowsalot",
+    "species": "cat",
+    "favFood": "tuna"
+};
+//myCat.favFood
+
+
+
+
+// arrays
+var myFavColours = ["blue", "green", "yellow", "red"];
+//myFavColours[1]
