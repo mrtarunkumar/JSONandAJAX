672e5ce60ff74099140291b8b80b0f1e177b8103
mrtarunkumar <Tarunkumar.Vegamwala@experian.com>
2017-03-20 15:49:10 +0000

    objects and arrays - json request - parse json string

--- main.js
+++ main.js
@@ -7,8 +7,9 @@ var ourRequest = new XMLHttpRequest();
 // we want to get data from our json
 ourRequest.open('GET', 'http://localhost:3000/json/data1.json');
 ourRequest.onload = function () {
-    //quick test to check our json data
-    console.log(ourRequest.responseText);
+    //assign our request to a variable
+    var ourData = JSON.parse(ourRequest.responseText);
+    console.log(ourData[0]);
 };
 
 // Send the request.
