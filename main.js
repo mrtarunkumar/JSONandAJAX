7cae8dcb497cf0b8dbc5b83e15d097585ccb816c
mrtarunkumar <Tarunkumar.Vegamwala@experian.com>
2017-03-20 15:43:05 +0000

    objects and arrays - json request

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-// array with objects //JSON
-var thePets = [
-    {
-        "name": "Meowsalot",
-        "species": "cat",
-        "favFood": "tuna"
-    },
-    {
-        "name": "Barksalot",
-        "species": "dog",
-        "favFood": "carrots"
-    }
-];
+// USE XMLHttpRequest
+var ourRequest = new XMLHttpRequest();
 
-//thePets[1].favFood
+// We can get or send data
+// to get data we use GET
+// to send data we can use POST
+// we want to get data from our json
+ourRequest.open('GET', 'http://localhost:3000/json/data1.json');
+ourRequest.onload = function () {
+    //quick test to check our json data
+    console.log(ourRequest.responseText);
+};
 
+// Send the request.
+ourRequest.send();
