88413f228d03ff55b1835464eaf5f6f31b847a04
mrtarunkumar <Tarunkumar.Vegamwala@experian.com>
2017-03-23 13:14:42 +0000

    objects and arrays - json request - parse json url 3

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ btn.addEventListener("click", function () {
     // Send the request.
     ourRequest.send();
     pageCounter = pageCounter + 1;
-    if(pageCounter > 3){
+    if (pageCounter > 3) {
         btn.classList.add("hideme");
     }
 });
@@ -33,7 +33,26 @@ btn.addEventListener("click", function () {
 function renderHTML(data) {
     var htmlString = "";
     for (i = 0; i < data.length; i++) {
-        htmlString += "<p>" + data[i].name + " is a " + data[i].species + ".</p>";
+        htmlString += "<p>" + data[i].name + " is a " + data[i].species + " that likes to eat ";
+        for(ii = 0; ii < data[i].foods.likes.length; ii++){
+           if (ii == 0) {
+               htmlString += data[i].foods.likes[ii]; 
+           } else {
+               htmlString += " and " + data[i].foods.likes[ii]; 
+           }
+        }
+        
+        htmlString += " and dislikes ";
+        
+        for(ii = 0; ii < data[i].foods.dislikes.length; ii++){
+           if (ii == 0) {
+               htmlString += data[i].foods.dislikes[ii]; 
+           } else {
+               htmlString += " and " + data[i].foods.dislikes[ii]; 
+           }
+        }
+        
+        htmlString += ".</p>";
     } ;
     newDataContainer.insertAdjacentHTML('beforeend', htmlString);
 } ;
