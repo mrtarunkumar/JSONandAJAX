2dd6de1c8468dfbb2175200777d4b1023464c459
mrtarunkumar <Tarunkumar.Vegamwala@experian.com>
2017-03-23 12:16:51 +0000

    objects and arrays - json request - parse json url 1

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
-// USE XMLHttpRequest
-var ourRequest = new XMLHttpRequest();
+var newDataContainer = document.getElementById("newData");
+var btn = document.getElementById("ftcData");
 
-// We can get or send data
-// to get data we use GET
-// to send data we can use POST
-// we want to get data from our json
-ourRequest.open('GET', 'http://localhost:3000/json/data1.json');
-ourRequest.onload = function () {
-    //assign our request to a variable
-    var ourData = JSON.parse(ourRequest.responseText);
-    console.log(ourData[0]);
-};
+btn.addEventListener("click", function () {
+    // USE XMLHttpRequest
+    var ourRequest = new XMLHttpRequest();
 
-// Send the request.
-ourRequest.send();
+    // We can get or send data
+    // to get data we use GET
+    // to send data we can use POST
+    // we want to get data from our json
+    ourRequest.open('GET', 'http://localhost:3000/json/data1.json');
+    ourRequest.onload = function () {
+        //assign our request to a variable
+        var ourData = JSON.parse(ourRequest.responseText);
+        console.log(ourData[0]);
+        
+        //Pass ourData to renderHTML function
+        renderHTML(ourData);
+    };
+
+    // Send the request.
+    ourRequest.send();
+});
+
+
+// Function to render data to the page
+function renderHTML(data) {
+    var htmlString = "";
+    for (i = 0; i < data.length; i++) {
+        htmlString += "<p>" + data[i].name + " is a " + data[i].species + ".</p>";
+    } ;
+    newDataContainer.insertAdjacentHTML('beforeend', htmlString);
+} ;
